902aaa77c73b79f5d491eb0842856629f5a12395
Karina Grishaeva <revuwem@gmail.com>
2023-01-05 18:38:30 +0400

    feat(tailwind): extend theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: "hsl(0, 0%, 100%)",
+        cyan: "hsl(158, 36%, 37%)",
+        cream: "hsl(30, 38%, 92%)",
+        blue: {
+          100: "hsl(212, 21%, 14%)",
+          200: "hsl(228, 12%, 48%)",
+        },
+      },
+      fontFamily: {
+        sans: ["Montserrat", ...defaultTheme.fontFamily.sans],
+        serif: ["Fraunces", ...defaultTheme.fontFamily.serif],
+      },
+    },
   },
   plugins: [],
 };
