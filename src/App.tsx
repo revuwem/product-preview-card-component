b326bd11999d59cfca2ffedf61c4e6ae4d9097d2
Karina Grishaeva <revuwem@gmail.com>
2023-01-06 00:03:49 +0400

    feat: create App layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import Card from "./components/Card";
+import Attribution from "./components/Attribution";
+
 function App() {
-  return <>app</>;
+  return (
+    <main className="bg-cream min-h-screen grid place-items-center gap-4 p-4">
+      <Card />
+      <Attribution />
+    </main>
+  );
 }
 
 export default App;
