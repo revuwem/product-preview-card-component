c5487ec7ba89533f7d45c4f49460ad4f98679779
Karina Grishaeva <revuwem@gmail.com>
2023-01-06 00:58:49 +0400

    feat(Button): add text color

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,7 +6,7 @@ const Button: React.FC<IButtonProps> = ({ children, ...props }) => {
   return (
     <button
       {...props}
-      className="bg-cyan-200 w-full flex justify-center items-center gap-3 p-3 font-bold rounded-xl hover:bg-cyan-100 transition"
+      className="bg-cyan-200 w-full flex justify-center items-center gap-3 p-3 font-bold text-white rounded-xl hover:bg-cyan-100 transition"
     >
       {children}
     </button>
